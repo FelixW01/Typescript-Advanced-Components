f2e6c64e4817b858530b78075fd3b87ee0c7615e
Felix W <felixwillem01@yahoo.com>
2023-12-07 19:50:25 -0500

    worked with reusable button components depending on the type of el.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import Input from "./components/input.tsx";
+// import Input from "./components/Input.tsx";
+import Button from "./components/Button.tsx";
 
 function App() {
   return (
     <main>
-      <Input id="name" label="your name" type="text" disabled />
-      <Input id="age" label="your age" type="number" />
+      <p>
+        <Button el="button">A Button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">
+          A Link
+        </Button>
+      </p>
     </main>
   );
 }
