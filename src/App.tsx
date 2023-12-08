4050798532078555d6b291b83a875593c7d9f624
Felix W <felixwillem01@yahoo.com>
2023-12-07 19:11:33 -0500

    added a dynamic reusable input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Input from "./components/input.tsx";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <Input id="name" label="your name" type="text" disabled />
+      <Input id="age" label="your age" type="number" />
+    </main>
+  );
 }
 
 export default App;
